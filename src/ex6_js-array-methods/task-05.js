511cb208e099135166fb656c1b3901d7284e6b0b
Kate Zhavoronkova <ikotichek@gmail.com>
2022-03-29 11:37:13 +0300

    6-th exercise to review

---
+++ src/ex6_js-array-methods/task-05.js
@@ -0,0 +1,11 @@
+function map(array, callback) {
+  const mappedArray = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    mappedArray.push(callback(array[i], i, array));
+  }
+
+  return mappedArray;
+}
+
+module.exports = map;
